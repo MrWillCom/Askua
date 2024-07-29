274d6597132b0254a5b5aee10667f734ffc11c21
Mr. Will <mr.will.com@outlook.com>
2024-07-29 12:36:42 +0800

    Add custom verification email

---
+++ src/auth.tsx
@@ -0,0 +1,40 @@
+import NextAuth from 'next-auth'
+import { PrismaAdapter } from '@auth/prisma-adapter'
+import { PrismaClient } from '@prisma/client'
+import Resend from 'next-auth/providers/resend'
+import { render } from '@react-email/render'
+import VerificationRequestEmail from './emails/VerificationRequestEmail'
+
+const prisma = new PrismaClient()
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    Resend({
+      from: 'Askua <noreply@mrwillcom.com>',
+      sendVerificationRequest: async ({ identifier, provider, url }) => {
+        const { host } = new URL(url)
+
+        const res = await fetch('https://api.resend.com/emails', {
+          method: 'POST',
+          headers: {
+            Authorization: `Bearer ${provider.apiKey}`,
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            from: provider.from,
+            to: identifier,
+            subject: `Sign in to Askua`,
+            html: render(<VerificationRequestEmail url={url} host={host} />, {
+              pretty: true,
+            }),
+            text: `Sign in to Askua (${host})\n${url}\n\n`,
+          }),
+        })
+
+        if (!res.ok)
+          throw new Error('Resend error: ' + JSON.stringify(await res.json()))
+      },
+    }),
+  ],
+})
---
+++ src/emails/VerificationRequestEmail.tsx
@@ -0,0 +1,46 @@
+import {
+  Body,
+  Button,
+  Container,
+  Font,
+  Head,
+  Heading,
+  Html,
+  Preview,
+  Text,
+} from '@react-email/components'
+import * as React from 'react'
+
+interface VerificationRequestEmailProps {
+  url: string
+  host: string
+}
+
+export const VerificationRequestEmail = ({
+  url,
+  host,
+}: VerificationRequestEmailProps) => {
+  return (
+    <Html>
+      <Head>
+        <Font fontFamily="system-ui" fallbackFontFamily="sans-serif" />
+      </Head>
+      <Preview>Click the button to sign in to Askua ({host}).</Preview>
+      <Body>
+        <Container>
+          <Heading>Sign in to Askua</Heading>
+          <Text>
+            This verification request comes from the Askua server on {host}. Tap
+            the following button to sign in to Askua:
+          </Text>
+          <Button href={url}>Sign in</Button>
+          <Text>
+            If you did not request this email you can safely ignore it.
+          </Text>
+        </Container>
+      </Body>
+    </Html>
+  )
+}
+
+export default VerificationRequestEmail
