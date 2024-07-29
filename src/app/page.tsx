fc6e9b2a7ad42f654f6924c09b75d5862c06efa7
Mr. Will <mr.will.com@outlook.com>
2024-07-29 11:37:33 +0800

    Add magic link sign in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import { SignIn } from './components/sign-in'
 import styles from './page.module.scss'
 
 export default function Home() {
-  return <>Hello, World!</>
+  return (
+    <>
+      <h1>Hello, World!</h1>
+      <SignIn />
+    </>
+  )
 }
--- src/auth.ts
+++ src/auth.ts
@@ -1,10 +1,15 @@
 import NextAuth from 'next-auth'
 import { PrismaAdapter } from '@auth/prisma-adapter'
 import { PrismaClient } from '@prisma/client'
+import Resend from 'next-auth/providers/resend'
 
 const prisma = new PrismaClient()
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [],
+  providers: [
+    Resend({
+      from: 'Askua <noreply@mrwillcom.com>',
+    }),
+  ],
 })
