adc58d2ffb51075206f40d3696d858ddbb7f6154
Mr. Will <mr.will.com@outlook.com>
2024-07-29 11:19:59 +0800

    Add Next Auth and its Prisma adapter

---
+++ src/auth.ts
@@ -0,0 +1,10 @@
+import NextAuth from 'next-auth'
+import { PrismaAdapter } from '@auth/prisma-adapter'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [],
+})
