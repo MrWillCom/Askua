b797d02f63a55e00391d7b758190e0d1a1415bc3
Mr. Will <mr.will.com@outlook.com>
2024-07-29 20:26:58 +0800

    Update middleware protecting paths

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,1 +1,10 @@
-export { auth as middleware } from '@/auth'
+import { auth } from '@/auth'
+
+export default auth(request => {
+  const pathname = request.nextUrl.pathname
+  const isProtected = ['/settings'].some(v => pathname.startsWith(v))
+
+  if (!request.auth && isProtected) {
+    return Response.redirect(new URL('/sign-in', request.nextUrl.origin))
+  }
+})
