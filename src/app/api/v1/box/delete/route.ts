b7e614320f612699267f9c98780eb97a600c9755
Mr. Will <mr.will.com@outlook.com>
2024-07-31 16:34:13 +0800

    Add box delete route

---
+++ src/app/api/v1/box/delete/route.ts
@@ -0,0 +1,43 @@
+import { NextRequest } from 'next/server'
+import prisma from '@/utils/db'
+import { isCuid } from '@paralleldrive/cuid2'
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library'
+
+export async function DELETE(request: NextRequest) {
+  const searchParams = new URL(request.url).searchParams
+  const id = searchParams.get('id')
+  const identifier = searchParams.get('identifier')
+
+  if (
+    (typeof id === 'string' ? !isCuid(id) : true) &&
+    typeof identifier !== 'string'
+  ) {
+    return Response.json(
+      { error: 'Both `id` and `identifier` are invalid.' },
+      { status: 400 },
+    )
+  }
+
+  try {
+    const box = await prisma.box.delete({
+      where: { id: id ?? undefined, identifier: identifier ?? undefined },
+    })
+
+    return Response.json(box, { status: 200 })
+  } catch (error) {
+    if ((error as PrismaClientKnownRequestError).code === 'P2025') {
+      return Response.json(
+        {
+          error: "There isn't any Box matching the given `id` or `identifier`.",
+        },
+        { status: 400 },
+      )
+    }
+
+    console.error(error)
+    return Response.json(
+      { error: 'Unhandled error happened on server-side.' },
+      { status: 500 },
+    )
+  }
+}
