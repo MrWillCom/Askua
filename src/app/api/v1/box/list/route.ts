0e49b2bc28dc5c71127346322da5d1b975ecd77b
Mr. Will <mr.will.com@outlook.com>
2024-07-31 14:49:58 +0800

    Update box list route to include authorization check for retrieving public boxes

--- src/app/api/v1/box/list/route.ts
+++ src/app/api/v1/box/list/route.ts
@@ -1,8 +1,12 @@
 import { NextRequest } from 'next/server'
 import prisma from '@/utils/db'
+import isAuthorized from '@/utils/isAuthorized'
 
 export async function GET(request: NextRequest) {
+  const authorized = isAuthorized(request)
+
   const boxes = await prisma.box.findMany({
+    where: authorized ? {} : { public: true },
     include: {
       questions: { select: { id: true }, orderBy: { createdAt: 'desc' } },
     },
