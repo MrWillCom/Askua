4a2297f4f28a540d0c658d37d0d921efae932de9
Mr. Will <mr.will.com@outlook.com>
2024-07-31 12:30:46 +0800

    Update question update route to include authorization check

--- src/app/api/v1/question/update/route.ts
+++ src/app/api/v1/question/update/route.ts
@@ -1,53 +1,58 @@
 import { NextRequest } from 'next/server'
 import prisma from '@/utils/db'
 import { isCuid } from '@paralleldrive/cuid2'
+import isAuthorized, { unauthorizedResponse } from '@/utils/isAuthorized'
 
 export async function PUT(request: NextRequest) {
-  const formData = await request.formData()
-  const id = formData.get('id')
-  const content = formData.get('content')
-  const reply = formData.get('reply')
+  if (isAuthorized(request)) {
+    const formData = await request.formData()
+    const id = formData.get('id')
+    const content = formData.get('content')
+    const reply = formData.get('reply')
 
-  if (typeof id === 'string' ? !isCuid(id) : true) {
-    return Response.json({ error: 'Given `id` is invalid.' }, { status: 400 })
-  }
-
-  try {
-    const oldQuestion = await prisma.question.findUniqueOrThrow({
-      where: { id: id as string },
-    })
-
-    let modifications = {}
-
-    if (typeof content === 'string') {
-      modifications = { ...modifications, content }
+    if (typeof id === 'string' ? !isCuid(id) : true) {
+      return Response.json({ error: 'Given `id` is invalid.' }, { status: 400 })
     }
 
-    if (typeof reply === 'string') {
-      if (oldQuestion.repliedAt === null) {
-        modifications = { ...modifications, repliedAt: new Date() }
+    try {
+      const oldQuestion = await prisma.question.findUniqueOrThrow({
+        where: { id: id as string },
+      })
+
+      let modifications = {}
+
+      if (typeof content === 'string') {
+        modifications = { ...modifications, content }
       }
-      modifications = { ...modifications, reply }
-    }
 
-    const newQuestion = await prisma.question.update({
-      where: { id: oldQuestion.id },
-      data: modifications,
-    })
+      if (typeof reply === 'string') {
+        if (oldQuestion.repliedAt === null) {
+          modifications = { ...modifications, repliedAt: new Date() }
+        }
+        modifications = { ...modifications, reply }
+      }
 
-    return Response.json(newQuestion, { status: 200 })
-  } catch (error) {
-    if ((error as any)?.code === 'P2003') {
+      const newQuestion = await prisma.question.update({
+        where: { id: oldQuestion.id },
+        data: modifications,
+      })
+
+      return Response.json(newQuestion, { status: 200 })
+    } catch (error) {
+      if ((error as any)?.code === 'P2003') {
+        return Response.json(
+          { error: "There isn't any Question matching the given `id`." },
+          { status: 400 },
+        )
+      }
+
+      console.error(error)
       return Response.json(
-        { error: "There isn't any Question matching the given `id`." },
-        { status: 400 },
+        { error: 'Unhandled error happened on server-side.' },
+        { status: 500 },
       )
     }
-
-    console.error(error)
-    return Response.json(
-      { error: 'Unhandled error happened on server-side.' },
-      { status: 500 },
-    )
+  } else {
+    return unauthorizedResponse
   }
 }
